2ed74bcb76b555b9e3f655c265244f7571f632b5
ZOHAN STANIKZAI <Fahim>
2019-12-19 20:21:32 -0500

    this is class practice

---
+++ src/main/java/com/test/maven/com/test/maven/App.java
@@ -0,0 +1,13 @@
+package com.test.maven.com.test.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/test/maven/com/test/maven/Test.java
@@ -0,0 +1,10 @@
+package com.test.maven.com.test.maven;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+	
+	System.out.println("This is Test");}
+
+}
